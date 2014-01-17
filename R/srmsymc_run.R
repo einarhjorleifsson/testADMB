94bc256e3c1b1ed9a183ced3a438156d345223dc
einarhjorleifsson <einar.hjorleifsson@gmail.com>
2014-01-17 17:57:45 +0000

    simplify

--- R/srmsymc_run.R
+++ R/srmsymc_run.R
@@ -1,24 +1,3 @@
-#' @title Compile the srmsymc ADMB code
-#' 
-#' @description The \code{plotMSY} approach uses a ADModel Builder code. ...
-#' 
-#' @export
-#' 
-
-srmsymc_compile <- function() 
-{
-  cmd <- paste('cp',paste(path.package("msy"),'tpl/srmsymc.tpl',sep='/'),'.')
-  system(cmd)
-  #system('admb srmsymc')
-  cmd <- paste('cp',paste(path.package("msy"),'tpl/srmsymc2.tpl',sep='/'),'.')
-  system(cmd)
-  #system('admb srmsymc2')
-  compile_admb("srmsymc")
-  clean_admb("srmsymc")
-  compile_admb("srmsymc2")
-  clean_admb("srmsymc2")
-}
-
 #' @title Run srmsymc
 #' 
 #' @description Run the ADM srmsymc program
@@ -34,12 +13,13 @@ srmsymc_compile <- function()
 #' @param path characters. Name of the directory for storing the output. If
 #' missing, the output remains in the working directory.
 #' @param output boolean. If FALSE (default) no results (*.dat) files are returned.
+#' @param windose boolean for operating system, winows vs unix-like
 
 
-srmsymc_run <- function(sr,opt_sen=1,opt_pen=1,nits=100,path,output=FALSE) {
+srmsymc_run <- function(sr,opt_sen=1,opt_pen=1,nits=100,path,output=FALSE,windose=TRUE) {
   
   # check if file exists
-  if (!file.exists('srmsymc')) srmsymc_compile()
+  #if (!file.exists('srmsymc')) srmsymc_compile()
   if (!file.exists('srmsymc.dat')) stop('The srmsymc.dat file must be in the working directory')
   if (!file.exists('age.dat')) stop('The age.dat file must be in the working directory')
   
@@ -59,27 +39,31 @@ srmsymc_run <- function(sr,opt_sen=1,opt_pen=1,nits=100,path,output=FALSE) {
   cat('21 1 ', nits, "\n", sep="", file = tmpfile)
   close(tmpfile)
   
-  system(paste('srmsymc -mcmc', (nits+11)*10000, '-mcsave 10000 -nosdmcmc'))
-  system("srmsymc -mceval")
-  
-  if(!missing(path)) {
-    
-    if (!file.exists(path)) {
-      cmd <- paste("mkdir",path)
-      system(cmd)
-    }
-    
-    cmd <- paste("cp *.dat ",path,"/.",sep="")
-    system(cmd)
+  if(windose) {
+    system(paste('srmsymc.exe -mcmc', (nits+11)*10000, '-mcsave 10000 -nosdmcmc'))
+    system("srmsymc.exe -mceval")
+  } else {
+    system(paste('srmsymc -mcmc', (nits+11)*10000, '-mcsave 10000 -nosdmcmc'))
+    system("srmsymc -mceval")
   }
+  #if(!missing(path)) {
+    
+  #  if (!file.exists(path)) {
+  #    cmd <- paste("mkdir",path)
+  #    system(cmd)
+  #  }
+    
+  #  cmd <- paste("cp *.dat ",path,"/.",sep="")
+  #  system(cmd)
+  #}
   
-  if(output) {
-    x <- list()
-    x$par <- srmsymc_read_par()
-    x$yield <- srmsymc_read_yield()
-    x$ssb <- srmsymc_read_ssb()
-    return(x)
-  }
+  #if(output) {
+  #  x <- list()
+  #  x$par <- srmsymc_read_par()
+  #  x$yield <- srmsymc_read_yield()
+  #  x$ssb <- srmsymc_read_ssb()
+  #  return(x)
+  #}
   
   # TODO: clean directory
 }
